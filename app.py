d54e9bc10cee6d883b0276bed91c6052dca1ee74
hrafnir <g@tgg.su>
2020-02-20 20:08:47 +0300

    skeleton for app.py

--- app.py
+++ app.py
@@ -8,5 +8,34 @@ def main():
     pass
 
 
+@app.route('/goals/<goal>/ ')
+def get_goal():
+    pass
+
+
+@app.route('/profiles/<id tutor>/ ')
+def get_tutor():
+    pass
+
+
+@app.route('/request/')
+def do_request():
+    pass
+
+
+@app.route('/request_done/')
+def req_done():
+    pass
+
+
+@app.route('/booking/<id учителя>/<день недели>/<время>/')
+def do_the_booking():
+    pass
+
+@app.route('/booking_done/ ')
+def booking_done():
+    pass
+
+
 if __name__ == '__main__':
     app.run(port=4999, debug=True)
